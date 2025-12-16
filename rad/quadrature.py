1f14e76e38e97880e6bfba8f444df2ea1fb6e5db
Joanna Piper Morgan <joannapipermorgan@gmail.com>
2025-12-16 10:24:32 -0800

    adding normaliztion

--- rad/quadrature.py
+++ rad/quadrature.py
@@ -4,8 +4,16 @@ Docstring for rad_standAlone
 
 import numpy as np
 
+
 class quadrature:
-    def __init__(self, N_angles ):
+    # A class called quadrature
+    def __init__(self, N_angles):
+        if (
+            N_angles < 0
+        ):  # if else errorrrrrrrrrrorrrrrrrrrrorrrrrrrrrrorr
+            # rrrrrrrrorrrrrrrrrrorrrrrrrrr
+            raise ValueError("degree must be a positive integer")
+
         self.N_angles = N_angles
         self.angles, self.weights = np.polynomial.legendre.leggauss(N_angles)
 
@@ -14,6 +22,16 @@ class quadrature:
 
         phi = (psi * self.weights).sum(axis=1)
 
-        return(phi)
+        return phi
 
+    def normalization(self, phi):
+        """
+        Docstring for normalization
 
+        :param phi: Vector to be normalized
+        :return: normalized vector
+        """
+
+        phi /= np.sum(phi)
+
+        return phi
